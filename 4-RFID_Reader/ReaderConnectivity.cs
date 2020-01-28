488b75ecaed4263933bbbb4ae5990ed305e56347
Hasmiou <17071005+Hasmiou@users.noreply.github.com>
2020-01-28 12:30:31 +0100

    init project folder

---
+++ 4-RFID_Reader/ReaderConnectivity.cs
@@ -0,0 +1,175 @@
+﻿// Copyright ©2017-2018 Impinj, Inc.All rights reserved.
+// You may use and modify this code under the terms of the Impinj Software Tools License & Disclaimer.
+// Visit the following link for full license details, or contact Impinj, Inc. for a copy of the Impinj Software Tools License & Disclaimer.
+//     https://support.impinj.com/hc/en-us/articles/360000468370-Software-Tools-License-Disclaimer
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Reader Connectivity example
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using Impinj.OctaneSdk;
+
+namespace OctaneSdkUseCases
+{
+    class Program
+    {
+        const string READER_HOSTNAME = "192.168.1.20";  // NEED to set to your speedway!
+        // Create an instance of the ImpinjReader class.
+        static ImpinjReader reader = new ImpinjReader();
+
+        static void ConnectToReader()
+        {
+            try
+            {
+                Console.WriteLine("Attempting to connect to {0} ({1}).",
+                    reader.Name, READER_HOSTNAME); 
+                
+                // The maximum number of connection attempts
+                // before throwing an exception.
+                reader.MaxConnectionAttempts = 15;
+                // Number of milliseconds before a 
+                // connection attempt times out.
+                reader.ConnectTimeout = 6000;
+                // Connect to the reader.
+                // Change the ReaderHostname constant in SolutionConstants.cs 
+                // to the IP address or hostname of your reader.
+                reader.Connect(READER_HOSTNAME);
+                Console.WriteLine("Successfully connected.");
+
+                // Tell the reader to send us any tag reports and 
+                // events we missed while we were disconnected.
+                reader.ResumeEventsAndReports();
+            }
+            catch (OctaneSdkException e)
+            {
+                Console.WriteLine("Failed to connect."); 
+                throw e;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Assign a name to the reader. 
+                // This will be used in tag reports. 
+                reader.Name = "My Reader #1";
+
+                // Connect to the reader.
+                ConnectToReader();
+
+                // Get the default settings.
+                // We'll use these as a starting point
+                // and then modify the settings we're 
+                // interested in.
+                Settings settings = reader.QueryDefaultSettings();
+
+                // Start the reader as soon as it's configured.
+                // This will allow it to run without a client connected.
+                settings.AutoStart.Mode = AutoStartMode.Immediate;
+                settings.AutoStop.Mode = AutoStopMode.None;
+
+                // Use Advanced GPO to set GPO #1 
+                // when an client (LLRP) connection is present.
+                settings.Gpos.GetGpo(1).Mode = GpoMode.LLRPConnectionStatus;
+
+                // Tell the reader to include the timestamp in all tag reports.
+                settings.Report.IncludeFirstSeenTime = true;
+                settings.Report.IncludeLastSeenTime = true;
+                settings.Report.IncludeSeenCount = true;
+
+                // If this application disconnects from the 
+                // reader, hold all tag reports and events.
+                settings.HoldReportsOnDisconnect = true;
+
+                // Enable keepalives.
+                settings.Keepalives.Enabled = true;
+                settings.Keepalives.PeriodInMs = 5000;
+
+                // Enable link monitor mode.
+                // If our application fails to reply to
+                // five consecutive keepalive messages,
+                // the reader will close the network connection.
+                settings.Keepalives.EnableLinkMonitorMode = true;
+                settings.Keepalives.LinkDownThreshold = 5;
+
+                // Assign an event handler that will be called
+                // when keepalive messages are received.
+                reader.KeepaliveReceived += OnKeepaliveReceived;
+
+                // Assign an event handler that will be called
+                // if the reader stops sending keepalives.
+                reader.ConnectionLost += OnConnectionLost;
+
+                // Apply the newly modified settings.
+                reader.ApplySettings(settings);
+
+                // Save the settings to the reader's 
+                // non-volatile memory. This will
+                // allow the settings to persist
+                // through a power cycle.
+                reader.SaveSettings();
+
+                // Assign the TagsReported event handler.
+                // This specifies which method to call
+                // when tags reports are available.
+                reader.TagsReported += OnTagsReported;
+
+                // Wait for the user to press enter.
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+
+                // Stop reading.
+                reader.Stop();
+
+                // Disconnect from the reader.
+                reader.Disconnect();
+            }
+            catch (OctaneSdkException e)
+            {
+                // Handle Octane SDK errors.
+                Console.WriteLine("Octane SDK exception: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                // Handle other .NET errors.
+                Console.WriteLine("Exception : {0}", e.Message);
+            }
+        }
+
+        static void OnConnectionLost(ImpinjReader reader)
+        {
+            // This event handler is called if the reader  
+            // stops sending keepalive messages (connection lost).
+            Console.WriteLine("Connection lost : {0} ({1})", reader.Name, reader.Address);
+
+            // Cleanup
+            reader.Disconnect();
+
+            // Try reconnecting to the reader
+            ConnectToReader();
+        }
+
+        static void OnKeepaliveReceived(ImpinjReader reader)
+        {
+            // This event handler is called when a keepalive 
+            // message is received from the reader.
+            Console.WriteLine("Keepalive received from {0} ({1})", reader.Name, reader.Address);
+        }
+
+        static void OnTagsReported(ImpinjReader sender, TagReport report)
+        {
+            // This event handler is called asynchronously 
+            // when tag reports are available.
+            // Loop through each tag in the report 
+            // and print the data.
+            foreach (Tag tag in report)
+            {
+                Console.WriteLine("EPC : {0} Timestamp : {1}", tag.Epc, tag.LastSeenTime);
+            }
+        }
+    }
+}
